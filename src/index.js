0f11982b9b2195faca337810b16c6085c5297044
timsilber <tim.silber@me.com>
2022-07-28 21:20:07 -0400

    Set up fetch calls

--- src/index.js
+++ src/index.js
@@ -2,3 +2,30 @@ import './index.html'
 import './components/reset.css'
 import './components/fonts.css'
 import './styles.css'
+
+
+const apiKey = 'd6c8af0fdba659975a8a7b235b99d8f8'
+const location = 'philadelphia'
+const limit = '5'
+const zipCode = '19102'
+
+const fetchLatitude = (url) => {
+
+fetch(url, {
+    mode: 'cors'
+})
+.then((response) => response.json())
+.then((data) => {
+    const response = document.createElement('div')
+    response.innerHTML= `<div>${JSON.stringify(data)}</div>`
+    document.body.append(response)
+})
+}
+
+
+fetchLatitude(`http://api.openweathermap.org/data/2.5/weather?q=${location}&limit=${limit}&appid=${apiKey}`)
+
+
+fetchLatitude(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${apiKey}`)
+
+fetchLatitude(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&limit=${limit}&appid=${apiKey}`)
