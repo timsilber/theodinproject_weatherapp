4c20aceb4856671a9f2574950914af3b000a8229
timsilber <tim.silber@me.com>
2022-07-28 21:34:48 -0400

    Refactor fetch to module

--- src/index.js
+++ src/index.js
@@ -9,23 +9,24 @@ const location = 'philadelphia'
 const limit = '5'
 const zipCode = '19102'
 
+const getWeatherData = (() => {
+
 const fetchLatitude = (url) => {
 
-fetch(url, {
-    mode: 'cors'
-})
-.then((response) => response.json())
-.then((data) => {
-    const response = document.createElement('div')
-    response.innerHTML= `<div>${JSON.stringify(data)}</div>`
-    document.body.append(response)
-})
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const response = document.createElement('div')
+        response.innerHTML= `<div>${JSON.stringify(data)}</div>`
+        document.body.append(response)})
 }
 
+    return {fetchLatitude}
+})();
 
-fetchLatitude(`http://api.openweathermap.org/data/2.5/weather?q=${location}&limit=${limit}&appid=${apiKey}`)
+getWeatherData.fetchLatitude(`http://api.openweathermap.org/data/2.5/weather?q=${location}&limit=${limit}&appid=${apiKey}`)
 
 
-fetchLatitude(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${apiKey}`)
+getWeatherData.fetchLatitude(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${limit}&appid=${apiKey}`)
 
-fetchLatitude(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&limit=${limit}&appid=${apiKey}`)
+getWeatherData.fetchLatitude(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&limit=${limit}&appid=${apiKey}`)
